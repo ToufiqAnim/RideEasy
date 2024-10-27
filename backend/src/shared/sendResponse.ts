14ae41276b235d0a1dba76cd7edc70f5cc655bfc
Toufiqul Islam Anim <toufiq9610@gmail.com>
2024-10-27 20:25:06 +0600

    catchAsync and sendResponse function created

---
+++ backend/src/shared/sendResponse.ts
@@ -0,0 +1,23 @@
+import { Response } from "express";
+
+type TApiResponse<T> = {
+  statusCode: number;
+  success: boolean;
+  message?: string | null;
+  meta?: {
+    page: number;
+    limit: number;
+    total: number;
+  };
+  data?: T | null;
+};
+export const sendResponse = <T>(res: Response, data: TApiResponse<T>): void => {
+  const responseData: TApiResponse<T> = {
+    statusCode: data.statusCode,
+    success: data.success,
+    message: data.message || null,
+    meta: data.meta || null || undefined,
+    data: data.data || null,
+  };
+  res.status(data.statusCode).json(responseData);
+};
