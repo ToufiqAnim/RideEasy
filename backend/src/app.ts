98743ad7d2fb3b0ed1565df339023e407116be5d
Toufiqul Islam Anim <toufiq9610@gmail.com>
2024-10-28 21:25:56 +0600

    global error handler created

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,7 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application, Request, Response, NextFunction } from "express";
 import cors from "cors";
 import router from "./app/routes";
+import globalErrorHandler from "./app/middleware/globalErrorHandler";
 
 const app: Application = express();
 
@@ -10,8 +11,24 @@ app.use(express.json());
 
 app.use("/api", router);
 
+// global error handler
+app.use(globalErrorHandler);
+
 app.get("/", (req: Request, res: Response) => {
   res.send("Welcome To Car Rental Backend !");
 });
-
+//handle not found
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    success: false,
+    message: "Not Found",
+    errorMessages: [
+      {
+        path: req.originalUrl,
+        message: "API Not Found",
+      },
+    ],
+  });
+  next();
+});
 export default app;
---
+++ backend/src/app/middleware/globalErrorHandler.ts
@@ -0,0 +1,64 @@
+import { ErrorRequestHandler, NextFunction, Request, Response } from "express";
+import { IGenericErrorMessage } from "../../interfaces/error";
+import handleValidationError from "../../errors/handleValidationError";
+import handleCastError from "../../errors/handleCastError";
+import { ZodError } from "zod";
+import handleZodError from "../../errors/handleZodError";
+
+import ApiError from "../../errors/ApiError";
+import config from "../../config";
+const globalErrorHandler: ErrorRequestHandler = (
+  error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  let statusCode = 500;
+  let message = "Somethng went wrong!!!";
+  let errorMessages: IGenericErrorMessage[] = [];
+
+  if (error?.name === "validationError") {
+    const simplifiedError = handleValidationError(error);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorMessages = simplifiedError.errorMessages;
+  } else if (error?.name === "castError") {
+    const simplifiedError = handleCastError(error);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorMessages = simplifiedError.errorMessages;
+  } else if (error instanceof ZodError) {
+    const simplifiedError = handleZodError(error);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorMessages = simplifiedError.errorMessages;
+  } else if (error instanceof ApiError) {
+    statusCode = error?.statusCode;
+    message = error.message;
+    errorMessages = error?.message
+      ? [
+          {
+            path: "",
+            message: error?.message,
+          },
+        ]
+      : [];
+  } else if (error instanceof Error) {
+    message = error?.message;
+    errorMessages = error?.message
+      ? [
+          {
+            path: "",
+            message: error?.message,
+          },
+        ]
+      : [];
+  }
+  res.status(statusCode).json({
+    success: false,
+    message,
+    errorMessages,
+    stack: config.env !== "production" ? error?.stack : undefined,
+  });
+};
+export default globalErrorHandler;
