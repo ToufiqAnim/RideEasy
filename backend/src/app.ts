2b1d4b758108548afcc34c36285a04d8775875bc
Toufiqul Islam Anim <toufiq9610@gmail.com>
2024-11-01 04:01:39 +0600

    cors edited

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -7,15 +7,16 @@ import cookieParser from "cookie-parser";
 const app: Application = express();
 
 const corsOptions = {
-  origin: "http://localhost:5173/",
+  origin: ["http://localhost:5173"],
   credentials: true,
 };
 
+app.use(express.json());
+app.use(cookieParser());
+
 app.use(cors(corsOptions));
 
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
 app.use("/api", router);
 import httpStatus = require("http-status");
 
